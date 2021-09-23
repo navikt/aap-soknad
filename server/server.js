1ac1b4427cfab9d1b2290b9cc8e1bfa9d27462bd
Tor Idland <tor.idland@nav.no>
2021-09-23 15:09:53 +0200

    health checks

--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ const startServer = () => {
     server.use(BASE_PATH, express.static(HOME_FOLDER));
 
     // health checks
-    server.get('/internal/isAlive', (req, res) => res.sendStatus(200));
-    server.get('/internal/isReady', (req, res) => res.sendStatus(200));
+    server.get(`${BASE_PATH}/internal/isAlive`, (req, res) => res.sendStatus(200));
+    server.get(`${BASE_PATH}/internal/isReady`, (req, res) => res.sendStatus(200));
 
     server.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, HOME_FOLDER, 'index.html'));
