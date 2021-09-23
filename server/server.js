533f4ceab989a08210f996d314b1d20ff2a6868b
Tor Idland <tor.idland@nav.no>
2021-09-23 15:21:57 +0200

    base_path

--- server/server.js
+++ server/server.js
@@ -13,20 +13,8 @@ const startServer = () => {
     server.get(`${BASE_PATH}/internal/isAlive`, (req, res) => res.sendStatus(200));
     server.get(`${BASE_PATH}/internal/isReady`, (req, res) => res.sendStatus(200));
 
-    server.get('/*', (req, res) => {
+    server.get(`${BASE_PATH}/*`, (req, res) => {
         res.sendFile(path.join(__dirname, HOME_FOLDER, 'index.html'));
-    });
-
-    server.use(function (req, res) {
-        // eslint-disable-next-line no-console
-        console.error('Server: Error 404', req.url);
-        res.status(404).send('404 not found');
-    });
-
-    server.use(function (err, req, res) {
-        // eslint-disable-next-line no-console
-        console.error('Server: Error 500', err);
-        res.status(500).send('500 Error');
     });
 
     server.listen(PORT, () => {
