fe8aa90608956e87e2d3e08f8adab8b72fb61ae1
Tor Idland <tor.idland@nav.no>
2021-09-30 09:49:00 +0200

    idporten token verify

---
+++ server/server.ts
@@ -0,0 +1,43 @@
+const express = require("express");
+const server = express();
+const path = require("path");
+const { validerToken } = require("./idporten");
+
+const BASE_PATH = "/aap";
+const BUILD_PATH = path.join(__dirname, "../build");
+const PORT = process.env.PORT || 3000;
+
+const startServer = () => {
+  server.use(express.static(BUILD_PATH));
+
+  // health checks
+  server.get(`${BASE_PATH}/internal/isAlive`, (req: any, res: any) =>
+    res.sendStatus(200)
+  );
+  server.get(`${BASE_PATH}/internal/isReady`, (req: any, res: any) =>
+    res.sendStatus(200)
+  );
+
+  server.use(`${BASE_PATH}/*`, async (req: any, res: any, next: any) => {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      res.redirect(`/oauth2/login?redirect=${BASE_PATH}/`);
+    } else {
+      const token = authorization.split(" ")[1];
+      await validerToken(token);
+      next();
+    }
+  });
+
+  server.get(`${BASE_PATH}/*`, (req: any, res: any) => {
+    res.sendFile(path.join(BUILD_PATH, "index.html"));
+  });
+
+  server.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log("Server: listening on port", PORT);
+  });
+};
+
+startServer();
