6cd528e6572406a1f55060de58fd9803e7a20c07
Tor Idland <tor.idland@nav.no>
2021-10-01 10:33:06 +0200

    separate dependencies for server

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.less";
 // @ts-ignore
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Heading } from "@navikt/ds-react";
 import Utland from "./pages/Utland";
 
 const App = (): JSX.Element => {
@@ -13,6 +14,9 @@ const App = (): JSX.Element => {
             <Utland />
           </Route>
           <Route path="*">
+            <Heading size={"2xlarge"} level={"1"} spacing={true}>
+              AAP App
+            </Heading>
             <span>Not Found</span>
           </Route>
         </Switch>
