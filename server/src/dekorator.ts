0ed5684b38ce4231b4cd85d13bceb545949aa063
Tor Idland <tor.idland@nav.no>
2021-10-20 15:24:57 +0200

    breadcrumbs and language selector

--- server/src/dekorator.ts
+++ server/src/dekorator.ts
@@ -1,9 +1,18 @@
 import { injectDecoratorServerSide } from "@navikt/nav-dekoratoren-moduler/ssr";
+import config from "./config";
 
 export const getHtmlWithDecorator = (filePath: string) =>
   injectDecoratorServerSide({
-    dekoratorenUrl: `${process.env.DECORATOR_URL}/?breadcrumbs=[{"url":"https://www.nav.no/person/dittnav","title":"Ditt NAV"},{"url":"https://www.nav.no/person/kontakt-oss","title":"Kontakt oss"}]&availableLanguages=[{"locale":"nb","url":"https://www.nav.no/person/kontakt-oss"},{"locale":"en","url":"https://www.nav.no/person/kontakt-oss/en/"}]`,
+    dekoratorenUrl: `${process.env.DECORATOR_URL}/?&availableLanguages=[{"locale":"nb","url":"https://www.nav.no/person/kontakt-oss"},{"locale":"en","url":"https://www.nav.no/person/kontakt-oss/en/"}]`,
     env: process.env.DECORATOR_ENV as any,
+    breadcrumbs: [
+      {url:'https://www.nav.no/person/dittnav',title:'Ditt NAV'},
+      {url: `${config.APP_URL}/aap/`,title: 'AAP'}
+    ],
+    availableLanguages: [
+      {locale: 'nb', handleInApp: true },
+      {locale: 'en', handleInApp: true },
+    ],
     filePath: filePath,
     simple: true,
     chatbot: false,
