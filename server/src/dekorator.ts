1d6be13028ab0a0a113f04cbc1a015ee2a5dc3ca
Tor Idland <tor.idland@nav.no>
2021-10-20 15:01:29 +0200

    breadcrumbs and language selector

--- server/src/dekorator.ts
+++ server/src/dekorator.ts
@@ -2,7 +2,7 @@ import { injectDecoratorServerSide } from "@navikt/nav-dekoratoren-moduler/ssr";
 
 export const getHtmlWithDecorator = (filePath: string) =>
   injectDecoratorServerSide({
-    dekoratorenUrl: process.env.DECORATOR_URL,
+    dekoratorenUrl: `${process.env.DECORATOR_URL}/?breadcrumbs=[{"url":"https://www.nav.no/person/dittnav","title":"Ditt NAV"},{"url":"https://www.nav.no/person/kontakt-oss","title":"Kontakt oss"}]&availableLanguages=[{"locale":"nb","url":"https://www.nav.no/person/kontakt-oss"},{"locale":"en","url":"https://www.nav.no/person/kontakt-oss/en/"}]`,
     env: process.env.DECORATOR_ENV as any,
     filePath: filePath,
     simple: true,
