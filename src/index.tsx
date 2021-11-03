7adc5a07402a8d4e5a4661cd3aed87688c9e159a
Tor Idland <tor.idland@nav.no>
2021-11-03 11:09:37 +0100

    Deploy to labs-gcp

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { setupLogger } from './utils/logger';
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.REACT_APP_USE_MOCK) {
   require('./mock/setupMock');
 }
 
