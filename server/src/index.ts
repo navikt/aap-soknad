db7ed66b9a9e00a922053facfcf2e7b8b0bb09cf
Tor Idland <tor.idland@nav.no>
2021-11-19 13:27:18 +0100

    prometheus metrics

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
+import client from 'prom-client';
 import config from './config';
 import { getHtmlWithDecorator } from "./dekorator";
 import { tokenXProxy} from "./apiProxy";
@@ -8,14 +9,28 @@ import {LogError, LogInfo} from "./logger";
 import {loginserviceCallback} from "./auth/loginservice";
 import {enforceIDPortenAuthenticationMiddleware} from "./auth/middleware";
 
+
 const BUILD_PATH = path.join(__dirname, "../build");
 const PORT = process.env.PORT || 3000;
 const server = express();
 
 const startServer = () => {
+  // Create a Registry which registers the metrics
+  const register = new client.Registry();
+  // Add a default label which is added to all metrics
+  register.setDefaultLabels({
+    app: 'aap-soknad'
+  });// Enable the collection of default metrics
+  client.collectDefaultMetrics({ register });
+
   server.use(cookieParser());
   server.use(express.static(BUILD_PATH));
 
+  // metrics
+  server.get(`${config.BASE_PATH}/internal/metrics`, (req: any, res: any) => {
+    res.setHeader('Content-Type', register.contentType);
+    res.end(register.metrics());
+  });
   // health checks
   server.get([`${config.BASE_PATH}/internal/isAlive`, `${config.BASE_PATH}/internal/isReady`], (req: any, res: any) =>
     res.sendStatus(200)
