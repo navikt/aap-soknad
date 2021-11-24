cc85faef7b114f316c00da57659078635c526d8b
tidnav <90849097+tidnav@users.noreply.github.com>
2021-11-24 08:43:23 +0100

    Feature/server improvements (#11)
    * logging with winston and improved testing
    
    * prometheus metrics
    
    * register.metrics is asymc
    
    * fix comments from PR

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,36 @@
 import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
+import client from 'prom-client';
 import config from './config';
-import { enforceIDPortenAuthenticationMiddleware } from './auth';
 import { getHtmlWithDecorator } from "./dekorator";
 import { tokenXProxy} from "./apiProxy";
+import {LogError, LogInfo} from "./logger";
+import {loginserviceCallback} from "./auth/loginservice";
+import {enforceIDPortenAuthenticationMiddleware} from "./auth/middleware";
+
 
 const BUILD_PATH = path.join(__dirname, "../build");
 const PORT = process.env.PORT || 3000;
 const server = express();
 
 const startServer = () => {
+  // Create a Registry which registers the metrics
+  const register = new client.Registry();
+  // Add a default label which is added to all metrics
+  register.setDefaultLabels({
+    app: 'aap-soknad'
+  });// Enable the collection of default metrics
+  client.collectDefaultMetrics({ register });
+
   server.use(cookieParser());
   server.use(express.static(BUILD_PATH));
 
+  // metrics
+  server.get(`${config.BASE_PATH}/internal/metrics`, async (req: any, res: any) => {
+    res.setHeader('Content-Type', register.contentType);
+    res.end(await register.metrics());
+  });
   // health checks
   server.get([`${config.BASE_PATH}/internal/isAlive`, `${config.BASE_PATH}/internal/isReady`], (req: any, res: any) =>
     res.sendStatus(200)
@@ -22,7 +39,7 @@ const startServer = () => {
   // Callback from loginservice, get originalUrl from cookie
   server.use(`${config.BASE_PATH}/loginservice`, loginserviceCallback);
 
-  // Enforce authentication
+  // Enforce idporten authentication
   server.use(`${config.BASE_PATH}/*`, enforceIDPortenAuthenticationMiddleware);
 
   // Reverse proxy to add tokenx header for api calls
@@ -35,20 +52,14 @@ const startServer = () => {
         res.send(html);
       })
       .catch((e) => {
+        LogError('Dekoratøren error', e);
         res.status(500).send(e);
       })
   );
 
   server.listen(PORT, () => {
-    // eslint-disable-next-line no-console
-    console.log("Server started: listening on port", PORT);
+    LogInfo(`Server started: listening on port ${PORT}`);
   });
-};
-
-const loginserviceCallback = async (req: any, res: any) => {
-  const path = `${req.cookies["APP_PATH"] || ""}`;
-  res.cookie("APP_PATH", "", { httpOnly: true, domain: "nav.no" });
-  res.redirect(path);
 };
 
 startServer();
