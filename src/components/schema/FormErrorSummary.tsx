639f424e369ae4d1aea9d4f1e5031331d28ff8c0
Øivind Stensrud <oivind.stensrud@nav.no>
2022-01-11 09:14:06 +0100

    Test for FormErrorSummary

--- src/components/schema/FormErrorSummary.tsx
+++ src/components/schema/FormErrorSummary.tsx
@@ -1,12 +1,14 @@
 import { FieldErrors } from "react-hook-form";
 import { ErrorSummary } from "@navikt/ds-react";
 import React from "react";
+import useTexts from "../../hooks/useTexts";
 
 const FormErrorSummary = ({ errors }: FieldErrors) => {
   const keyList = Object.keys(errors).filter((e) => e);
+  const {getText} = useTexts('felles');
   if (keyList.length < 1) return null;
   return (
-    <ErrorSummary>
+    <ErrorSummary heading={getText('skjemafeil')}>
       {keyList.map((key) => (
         <ErrorSummary.Item key={key} href={`#${key}`}>
           {
