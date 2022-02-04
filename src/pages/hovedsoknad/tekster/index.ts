91ec34a4958a1656053f2c9efd3ba1b9719ad942
Tor Idland <tor.idland@nav.no>
2022-02-04 15:15:45 +0100

    ø to o

--- src/pages/hovedsoknad/tekster/index.ts
+++ src/pages/hovedsoknad/tekster/index.ts
@@ -1,4 +1,4 @@
-import en from './en_hovedsøknad.json';
-import nb from './no_hovedsøknad.json';
+import en from './en_hovedsoknad.json';
+import nb from './no_hovedsoknad.json';
 
 export {en, nb};
