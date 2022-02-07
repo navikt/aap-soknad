0e5b1d78b2fbebdfd6f35113e139dc71b2174954
Tor Idland <tor.idland@nav.no>
2022-02-07 15:23:14 +0100

    app url from config when testing

--- src/api/useFetch.ts
+++ src/api/useFetch.ts
@@ -1,65 +1,15 @@
-import { useState, useEffect } from 'react';
-
-export const useFetchGET = (url: string) => {
-  const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string>('');
-
-  useEffect(() => {
-    const abortController = new AbortController();
-    const fetchGet = async () => {
-      setIsLoading(true);
-      try {
-        const res = await fetch(url, {method: 'GET'});
-        const data = await res.json();
-        data && setData(data);
-        setIsLoading(false);
-      } catch (e) {
-        setError(`useFetchGET: ${e}`);
-        setIsLoading(false);
-      }
-    }
-    fetchGet();
-    return () => {
-      abortController.abort();
-    }
-  }, [url])
-  return {data, isLoading, error}
-};
-
-export const useFetchPOST = () => {
-  const [data, setData] = useState({ok: false, data: undefined});
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string>('');
-
-  const postFetch = async (url: string, payload: object) => {
-    setIsLoading(true);
-    try {
-      const res = await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(payload)
-      });
-      const data = await res.json();
-      data && setData({ok: res.ok, data});
-      setIsLoading(false);
-    } catch (e) {
-      setError(`useFetchPOST: ${e}`);
-      setIsLoading(false);
-    }
-  };
-  return {postResponse: data, isLoading, postFetch, error}
-};
-
-type Options = {
-  headers?: object;
-}
-export const fetchPOST = async (url: string, payload: object, opts: Options = {}) => {
+import { RequestOptions } from "http";
+import {APP_URL_TEST} from "../config";
+export const fetchPOST = async (url: string, payload: object, opts: RequestOptions = {}) => {
+  const completeUrl = process.env.NODE_ENV === "test"
+    ? `${APP_URL_TEST}${url}`
+    : url;
   const headers = {
     'Content-Type': 'application/json',
     ...(opts.headers ? opts.headers : {})
   }
   try {
-    const res = await fetch(url, {
+    const res = await fetch(completeUrl, {
       method: 'POST',
       body: JSON.stringify(payload),
       headers
---
+++ src/hooks/useFetchGet.tsx
@@ -0,0 +1,33 @@
+import {useEffect, useState} from "react";
+import {APP_URL_TEST} from "../config";
+
+export const useFetchGET = (url: string) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
+
+  useEffect(() => {
+    if(!url) return;
+    const abortController = new AbortController();
+    const completeUrl = process.env.NODE_ENV === "test"
+      ? `${APP_URL_TEST}${url}`
+      : url;
+    const fetchGet = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(completeUrl, {method: 'GET'});
+        const data = await res.json();
+        data && setData(data);
+        setIsLoading(false);
+      } catch (e) {
+        setError(`useFetchGET: ${e}`);
+        setIsLoading(false);
+      }
+    }
+    fetchGet();
+    return () => {
+      abortController.abort();
+    }
+  }, [url])
+  return {data, isLoading, error}
+};
