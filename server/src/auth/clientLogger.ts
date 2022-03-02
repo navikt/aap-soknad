06bac998a546532b9e0a491a05ef4c10c0bdcffe
Tor Idland <tor.idland@nav.no>
2022-03-02 12:55:43 +0100

    clientlogger

--- server/src/auth/clientLogger.ts
+++ server/src/auth/clientLogger.ts
@@ -2,7 +2,10 @@ import { Request, Response } from 'express';
 import { LogError } from '../logger';
 
 export const logClientError = (req: Request, res: Response) => {
-  console.log(req?.body);
-  LogError('clientLogger', { error: req?.body?.error, stack: req?.body?.stack });
+  LogError(req?.body?.error, {
+    type: 'client error',
+    error: req?.body?.error,
+    stack: req?.body?.stack,
+  });
   res.sendStatus(200);
 };
