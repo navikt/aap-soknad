1c9bec9828e8d3a801ad8fc97f67ff30649b7625
Tor Idland <tor.idland@nav.no>
2022-03-02 12:12:42 +0100

    remove app.test

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('kan tegne appen', () => {
-  render(<App />);
-  expect(screen.getByText(/AAP App/)).toBeVisible();
-});
