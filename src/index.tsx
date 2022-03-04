019025f7dc73dcf123c37133a9db0a6bbbc31c7d
Tor Idland <tor.idland@nav.no>
2022-03-04 09:14:03 +0100

    sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import App from './App';
 import '@navikt/ds-css';
 
@@ -7,6 +9,16 @@ if (process.env.USE_MOCK === 'true') {
   const { worker } = require('./mocks/browser');
   worker.start();
 }
+
+Sentry.init({
+  dsn: 'https://816b41ce540d4024982ffbf3e3d9dac7@sentry.gc.nav.no/119',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const app = document.getElementById('app');
 render(
