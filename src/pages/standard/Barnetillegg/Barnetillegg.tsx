7010264504e8679aec1e250dd523e4a7bddae697
Tor Idland <tor.idland@nav.no>
2022-04-13 16:41:36 +0200

    Edit support - Barnetillegg

--- src/pages/standard/Barnetillegg/Barnetillegg.tsx
+++ src/pages/standard/Barnetillegg/Barnetillegg.tsx
@@ -7,6 +7,7 @@ import {
   GuidePanel,
   Heading,
   Ingress,
+  Label,
   Modal,
   Radio,
   RadioGroup,
@@ -19,7 +20,7 @@ import RadioGroupWrapper from '../../../components/input/RadioGroupWrapper';
 import { JaEllerNei } from '../../../types/Generic';
 import SoknadStandard from '../../../types/SoknadStandard';
 import * as classes from './Barnetillegg.module.css';
-import { Add, Delete } from '@navikt/ds-icons';
+import { Add } from '@navikt/ds-icons';
 import TextWithLink from '../../../components/TextWithLink';
 import {
   addRequiredVedlegg,
@@ -36,6 +37,7 @@ const BARNETILLEGG = 'barnetillegg';
 
 export const Barnetillegg = ({ getText, errors, control }: BarnetilleggProps) => {
   const { vedleggDispatch } = useVedleggContext();
+  const [selectedBarn, setSelectedBarn] = useState<number | undefined>(undefined);
   const [showModal, setShowModal] = useState<boolean>(false);
   const [fornavn, setFornavn] = useState<string>('');
   const [mellomnavn] = useState<string>('');
@@ -46,6 +48,50 @@ export const Barnetillegg = ({ getText, errors, control }: BarnetilleggProps) =>
     name: BARNETILLEGG,
     control,
   });
+  const clearModal = () => {
+    setFornavn('');
+    setEtternavn('');
+    setFnr('');
+    setHarInntekt('');
+  };
+  const editNyttBarn = (index: number) => {
+    setSelectedBarn(index);
+    const barn = fields[index];
+    setFornavn(barn?.navn?.fornavn);
+    setEtternavn(barn?.navn?.etternavn);
+    setFnr(barn?.fnr);
+    setHarInntekt(barn?.harInntekt?.value);
+    setShowModal(true);
+  };
+  const saveNyttBarn = () => {
+    if (!selectedBarn) {
+      append({
+        navn: { fornavn, mellomnavn, etternavn },
+        fnr,
+        harInntekt: { value: harInntekt },
+        manueltOpprettet: true,
+      });
+      addRequiredVedlegg(
+        [
+          {
+            type: `barn${fnr}`,
+            description: `Fødselsattest eller bostedsbevis for barn: ${fornavn} ${etternavn}`,
+          },
+        ],
+        vedleggDispatch
+      );
+    } else {
+      const barn = fields[selectedBarn];
+      update(selectedBarn, {
+        navn: { fornavn, mellomnavn, etternavn },
+        fnr,
+        harInntekt: { value: harInntekt },
+        manueltOpprettet: barn?.manueltOpprettet,
+      });
+    }
+    clearModal();
+    setShowModal(false);
+  };
   useEffect(() => {
     fields.forEach((field, index) => {
       if (!field?.harInntekt?.label)
@@ -77,42 +123,63 @@ export const Barnetillegg = ({ getText, errors, control }: BarnetilleggProps) =>
       {fields.map((barn, index) => {
         return (
           <article key={barn?.fnr} className={classes.barneKort}>
-            <div className={classes?.headingWrapper}>
-              <Heading
-                size={'xsmall'}
-                level={'2'}
-              >{`${barn?.navn?.fornavn} ${barn?.navn?.mellomnavn} ${barn?.navn?.etternavn}`}</Heading>
-              {barn?.manueltOpprettet && (
-                <Delete
-                  onClick={() => {
-                    removeRequiredVedlegg(`barn${barn?.fnr}`, vedleggDispatch);
-                    remove(index);
-                  }}
-                />
-              )}
-            </div>
+            <Heading
+              size={'xsmall'}
+              level={'2'}
+            >{`${barn?.navn?.fornavn} ${barn?.navn?.mellomnavn} ${barn?.navn?.etternavn}`}</Heading>
             <BodyShort>{`Fødselsnummer: ${barn?.fnr}`}</BodyShort>
-            <RadioGroupWrapper
-              legend={getText('form.barnetillegg.legend')}
-              name={`${BARNETILLEGG}.${index}.harInntekt.value`}
-              control={control}
-              error={errors?.[BARNETILLEGG]?.[index]?.erForsørger?.message}
-            >
-              <Radio value={JaEllerNei.JA}>
-                <BodyShort>{JaEllerNei.JA}</BodyShort>
-              </Radio>
-              <Radio value={JaEllerNei.NEI}>
-                <BodyShort>{JaEllerNei.NEI}</BodyShort>
-              </Radio>
-            </RadioGroupWrapper>
+            {barn?.manueltOpprettet && barn?.harInntekt ? (
+              <Grid>
+                <Cell xs={4}>
+                  <Label>{'Har inntekt over 1G:'}</Label>
+                </Cell>
+                <Cell xs={3}>
+                  <BodyShort>{barn?.harInntekt?.value}</BodyShort>
+                </Cell>
+              </Grid>
+            ) : (
+              <RadioGroupWrapper
+                legend={getText('form.barnetillegg.legend')}
+                name={`${BARNETILLEGG}.${index}.harInntekt.value`}
+                control={control}
+                error={errors?.[BARNETILLEGG]?.[index]?.erForsørger?.message}
+              >
+                <Radio value={JaEllerNei.JA}>
+                  <BodyShort>{JaEllerNei.JA}</BodyShort>
+                </Radio>
+                <Radio value={JaEllerNei.NEI}>
+                  <BodyShort>{JaEllerNei.NEI}</BodyShort>
+                </Radio>
+              </RadioGroupWrapper>
+            )}
+            {barn?.manueltOpprettet && (
+              <Grid>
+                <Cell xs={4}>
+                  <Button variant="tertiary" type="button" onClick={() => editNyttBarn(index)}>
+                    Endre
+                  </Button>
+                </Cell>
+              </Grid>
+            )}
           </article>
         );
       })}
       <BodyShort>{getText('steps.barnetillegg.leggTil.description')}</BodyShort>
-      <Button variant="secondary" type="button" onClick={() => setShowModal(true)}>
-        <Add />
-        Legg til barn
-      </Button>
+      <Grid>
+        <Cell xs={6}>
+          <Button
+            variant="tertiary"
+            type="button"
+            onClick={() => {
+              setSelectedBarn(undefined);
+              setShowModal(true);
+            }}
+          >
+            <Add />
+            Legg til barn
+          </Button>
+        </Cell>
+      </Grid>
       <Modal open={showModal} onClose={() => setShowModal(false)}>
         <Modal.Content className={classes?.leggTilBarnModalContent}>
           <Heading size={'medium'} level={'2'}>
@@ -120,21 +187,25 @@ export const Barnetillegg = ({ getText, errors, control }: BarnetilleggProps) =>
           </Heading>
           <Ingress>{getText('steps.barnetillegg.leggTil.description')}</Ingress>
           <TextField
+            value={fornavn}
             label={getText('form.barnetillegg.add.fornavn.label')}
             name={'fornavn'}
             onChange={(e) => e?.target?.value && setFornavn(e?.target?.value)}
           />
           <TextField
+            value={etternavn}
             label={getText('form.barnetillegg.add.etternavn.label')}
             name={'etternavn'}
             onChange={(e) => e?.target?.value && setEtternavn(e?.target?.value)}
           />
           <TextField
+            value={fnr}
             label={getText('form.barnetillegg.add.fnr.label')}
             name={'fnr'}
             onChange={(e) => e?.target?.value && setFnr(e?.target?.value)}
           />
           <RadioGroup
+            value={harInntekt}
             legend={getText('form.barnetillegg.legend')}
             name={'harInntekt'}
             onChange={(e) => e && setHarInntekt(e)}
@@ -166,30 +237,27 @@ export const Barnetillegg = ({ getText, errors, control }: BarnetilleggProps) =>
               </Button>
             </Cell>
             <Cell xs={3}>
-              <Button
-                type="button"
-                onClick={() => {
-                  append({
-                    navn: { fornavn, mellomnavn, etternavn },
-                    fnr,
-                    harInntekt,
-                    manueltOpprettet: true,
-                  });
-                  addRequiredVedlegg(
-                    [
-                      {
-                        type: `barn${fnr}`,
-                        description: `Fødselsattest eller bostedsbevis for barn: ${fornavn} ${etternavn}`,
-                      },
-                    ],
-                    vedleggDispatch
-                  );
-                  setShowModal(false);
-                }}
-              >
+              <Button type="button" onClick={() => saveNyttBarn()}>
                 Lagre
               </Button>
             </Cell>
+            {selectedBarn && (
+              <Cell xs={3}>
+                <Button
+                  type="button"
+                  variant={'danger'}
+                  onClick={() => {
+                    const barn = fields[selectedBarn];
+                    removeRequiredVedlegg(`barn${barn?.fnr}`, vedleggDispatch);
+                    remove(selectedBarn);
+                    clearModal();
+                    setShowModal(false);
+                  }}
+                >
+                  Slett
+                </Button>
+              </Cell>
+            )}
           </Grid>
         </Modal.Content>
       </Modal>
