9f0209ccb35372417e5269fa01864eb435ab062c
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-04-27 08:45:13 +0200

    Nextjs app skeleton, deploy til egen ingress

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { Heading } from '@navikt/ds-react';
+
+const Home = () => {
+  return (
+    <Heading level="1" size="xlarge">
+      Next app
+    </Heading>
+  );
+};
+
+export default Home;
