d8af3f0aa7e1775d95b78ab9cfab5d2d51996756
Tor Idland <tor.idland@nav.no>
2022-06-15 12:53:34 +0200

    fix testsøknad

--- src/pages/hovedsoknad/Hovedsoknad.tsx
+++ src/pages/hovedsoknad/Hovedsoknad.tsx
@@ -3,34 +3,23 @@ import React, { useState } from 'react';
 import { Alert, Button, GuidePanel, Loader, PageHeader } from '@navikt/ds-react';
 import { fetchPOST } from '../../api/fetch';
 import { useTexts } from '../../hooks/useTexts';
-import { StepWizard, Step } from '../../components/StepWizard';
 
 import * as tekster from './tekster';
-import { completeAndGoToNextStep, useStepWizard } from '../../context/stepWizardContextV2';
-
-enum StepName {
-  INTRODUCTION = 'INTRODUCTION',
-  RECEIPT = 'RECEIPT',
-}
 
 const Hovedsoknad = (): JSX.Element => {
-  const { stepWizardDispatch } = useStepWizard();
   const [innsendingFeil, setInnsendingFeil] = useState<boolean>(false);
   const [senderMelding, setSenderMelding] = useState<boolean>(false);
+  const [visKvittering, setVisKvittering] = useState<boolean>(false);
 
   const sendSøknad = async () => {
     setSenderMelding(true);
     const res = await fetchPOST('/aap/soknad-api/innsending/soknad', {});
+    setSenderMelding(false);
     if (!res.ok) {
-      setSenderMelding(false);
       setInnsendingFeil(true);
     }
     if (res.ok) {
-      if (innsendingFeil) {
-        setInnsendingFeil(false);
-        setSenderMelding(false);
-      }
-      completeAndGoToNextStep(stepWizardDispatch);
+      setVisKvittering(true);
     }
   };
 
@@ -39,21 +28,17 @@ const Hovedsoknad = (): JSX.Element => {
   return (
     <>
       <PageHeader>{getText('pageTitle')}</PageHeader>
-      <StepWizard>
-        <Step order={1} name={StepName.INTRODUCTION}>
-          <>
-            <GuidePanel poster>{getText('steps.introduction.guideText')}</GuidePanel>
-            <Button variant="primary" type="submit" onClick={sendSøknad} disabled={senderMelding}>
-              Søk AAP
-              {senderMelding && <Loader />}
-            </Button>
-          </>
-        </Step>
-        <Step order={2} name={StepName.RECEIPT}>
-          <GuidePanel>{getText('steps.kvittering')}</GuidePanel>
-        </Step>
-        {innsendingFeil && <Alert variant={'error'}>{getText('innsending.feil')}</Alert>}
-      </StepWizard>
+      {!visKvittering && (
+        <>
+          <GuidePanel poster>{getText('steps.introduction.guideText')}</GuidePanel>
+          <Button variant="primary" type="submit" onClick={sendSøknad} disabled={senderMelding}>
+            Søk AAP
+            {senderMelding && <Loader />}
+          </Button>
+        </>
+      )}
+      {visKvittering && <GuidePanel>{getText('steps.kvittering')}</GuidePanel>}
+      {innsendingFeil && <Alert variant={'error'}>{getText('innsending.feil')}</Alert>}
     </>
   );
 };
