236995ab2b766abe664e9f691eae42fc8a704080
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-06-21 09:24:23 +0200

    Komponent bruker ikke lengre getText

--- src/pages/standard/Veiledning/Veiledning.test.tsx
+++ src/pages/standard/Veiledning/Veiledning.test.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { Veiledning } from './Veiledning';
-import useTexts from '../../../hooks/useTexts';
-import * as tekster from '../tekster';
 import { axe, toHaveNoViolations } from 'jest-axe';
 expect.extend(toHaveNoViolations);
 
 describe('Veiledning', () => {
   const Component = () => {
-    const { getText } = useTexts(tekster);
     return (
       <>
-        <Veiledning onSubmit={jest.fn()} getText={getText} søker={{}} loading={false} />
+        <Veiledning onSubmit={jest.fn()} søker={{}} loading={false} />
       </>
     );
   };
