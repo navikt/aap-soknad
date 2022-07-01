3a9519225380245b075b1b7fc4931b59670abb12
Tor Idland <tor.idland@nav.no>
2022-07-01 09:08:01 +0200

    parse response as blob

--- src/pages/VedleggVisning.tsx
+++ src/pages/VedleggVisning.tsx
@@ -6,12 +6,12 @@ const VedleggVisning = () => {
   const { id } = useParams();
   useEffect(() => {
     const getFile = async () => {
-      const file = await fetch(`/aap/soknad-api/vedlegg/les/${id}`).then((res) => res.formData());
+      const file = await fetch(`/aap/soknad-api/vedlegg/les/${id}`).then((res) => res.blob());
       console.log('file', file);
       file && setFile(file);
     };
     getFile();
   }, [id]);
-  return file ? <img alt={'vedlegg'} src={URL?.createObjectURL(file)} /> : <></>;
+  return file ? <embed src={URL?.createObjectURL(file)} /> : <></>;
 };
 export default VedleggVisning;
