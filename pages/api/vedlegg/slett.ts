3e69740d2d48143531f539984f7172c5238eab00
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-07-21 15:34:19 +0200

    Returnerer riktig for sletting av vedlegg

--- pages/api/vedlegg/slett.ts
+++ pages/api/vedlegg/slett.ts
@@ -12,15 +12,16 @@ const handler = beskyttetApi(async (req: NextApiRequest, res: NextApiResponse) =
     res.status(400).json({ error: 'uuid må være en string' });
   }
   const accessToken = getAccessTokenFromRequest(req);
-  const result = await slettVedlegg(uuid as string, accessToken);
-  res.status(204).json({ result });
+  await slettVedlegg(uuid as string, accessToken);
+  res.status(204).json({});
 });
 
 export const slettVedlegg = async (uuid: string, accessToken?: string) => {
-  if (isMock()) return true;
-  return await tokenXProxy({
+  if (isMock()) return;
+  await tokenXProxy({
     url: `${process.env.SOKNAD_API_URL}/vedlegg/slett/${uuid}`,
     method: 'DELETE',
+    noResponse: true,
     audience: process.env.SOKNAD_API_AUDIENCE!,
     bearerToken: accessToken,
   });
