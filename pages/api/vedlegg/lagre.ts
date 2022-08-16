db99bf5350236027530c806c211203aed2ab7f66
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-08-16 11:59:00 +0200

    Logging av requester mot backend

--- pages/api/vedlegg/lagre.ts
+++ pages/api/vedlegg/lagre.ts
@@ -4,8 +4,10 @@ import { getAccessTokenFromRequest } from 'auth/accessToken';
 import { beskyttetApi } from 'auth/beskyttetApi';
 import { tokenXAxiosProxy } from 'auth/tokenXProxy';
 import { isMock } from 'utils/environments';
+import logger from 'utils/logger';
 
 const handler = beskyttetApi(async (req: NextApiRequest, res: NextApiResponse) => {
+  logger.info('Har mottatt request om filopplasting');
   const accessToken = getAccessTokenFromRequest(req);
   if (isMock()) {
     res.status(201).json(randomUUID());
