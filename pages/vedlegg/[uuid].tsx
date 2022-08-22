a30ef0715df4cc37cd67ad742933c6f2efd38970
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-08-22 10:30:49 +0200

    Reeimplementerer side for visning av vedlegg i nextjs

---
+++ pages/vedlegg/[uuid].tsx
@@ -0,0 +1,40 @@
+import { getAccessToken } from 'auth/accessToken';
+import { beskyttetSide } from 'auth/beskyttetSide';
+import { GetServerSidePropsResult, NextPageContext } from 'next';
+import { useRouter } from 'next/router';
+import { lesVedlegg } from 'pages/api/vedlegg/les';
+import { useEffect, useState } from 'react';
+import { getStringFromPossiblyArrayQuery } from 'utils/string';
+
+interface PageProps {}
+
+const Vedlegg = ({}: PageProps) => {
+  const router = useRouter();
+  const [file, setFile] = useState<Blob | undefined>(undefined);
+
+  const uuid = getStringFromPossiblyArrayQuery(router.query['uuid']);
+
+  useEffect(() => {
+    const getFile = async () => {
+      const file = await fetch(`/aap/soknad/api/vedlegg/les/?uuid=${uuid}`).then((res) =>
+        res.blob()
+      );
+      file && setFile(file);
+    };
+    getFile();
+  }, [uuid]);
+  if (file === undefined) {
+    return <h1>Loading...</h1>;
+  }
+  return <embed src={URL.createObjectURL(file)} />;
+};
+
+export const getServerSideProps = beskyttetSide(
+  async (ctx: NextPageContext): Promise<GetServerSidePropsResult<{}>> => {
+    return {
+      props: {},
+    };
+  }
+);
+
+export default Vedlegg;
