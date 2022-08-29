f8ff163f2f14be3162ee484fb011c4501fccbc0c
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-08-29 15:19:43 +0200

    Dropper pretty-logging inntil vi får det til å leke pent sammen med ecs logger i dev-miljø

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,24 +1,6 @@
 import pino from 'pino';
 import ecsFormat from '@elastic/ecs-pino-format';
 
-const localhostLogger = pino({
-  browser: {},
-  level: 'debug',
-  timestamp: false,
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-    },
-  },
-
-  base: {
-    env: process.env.NODE_ENV,
-  },
-});
-
 const ecsLogger = pino(ecsFormat());
 
-const logger = process.env.NEXT_PUBLIC_ENVIRONMENT === 'localhost' ? localhostLogger : ecsLogger;
-
-export default logger;
+export default ecsLogger;
