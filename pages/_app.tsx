34661459ed49c502b3fc18e2cca6dce067ccb1e0
Tor Idland <tor.idland@nav.no>
2022-09-02 15:35:28 +0200

    fjern sentry error boundary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '@navikt/ds-css';
 import 'styles/index.css';
 
 import React, { useEffect, useMemo } from 'react';
-import * as Sentry from '@sentry/react';
 import { IntlProvider } from 'react-intl';
 import { AppProps } from 'next/app';
 import { Modal } from '@navikt/ds-react';
@@ -12,7 +11,6 @@ import { messages, flattenMessages } from 'utils/message';
 import { SokerOppslagProvider } from 'context/sokerOppslagContext';
 import { StepWizardProvider } from 'context/stepWizardContextV2';
 import { initAmplitude } from 'utils/amplitude';
-import ErrorPage from 'components/error/ErrorPage';
 const CustomApp = ({ Component, pageProps }: AppProps) => {
   const locale = 'nb';
 
@@ -33,9 +31,7 @@ const CustomApp = ({ Component, pageProps }: AppProps) => {
     <IntlProvider locale={locale} messages={currentMessages}>
       <SokerOppslagProvider>
         <StepWizardProvider>
-          <Sentry.ErrorBoundary fallback={(props) => <ErrorPage {...props} />} showDialog={false}>
-            <Component {...pageProps} />
-          </Sentry.ErrorBoundary>
+          <Component {...pageProps} />
         </StepWizardProvider>
       </SokerOppslagProvider>
     </IntlProvider>
