eb77630599d2966e8f53f81dea129c17d896f108
Tor Idland <tor.idland@nav.no>
2022-09-14 20:06:47 +0200

    log level i logger

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,6 +1,13 @@
 import pino from 'pino';
 import ecsFormat from '@elastic/ecs-pino-format';
 
-const ecsLogger = pino(ecsFormat());
+const ecsLogger = pino({
+  ...ecsFormat(),
+  formatters: {
+    level: (label) => {
+      return { level: label };
+    },
+  },
+});
 
 export default ecsLogger;
