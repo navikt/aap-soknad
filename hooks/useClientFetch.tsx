43d62a50823d930c663a3b68086138c9fb7c0ce0
Tor Idland <tor.idland@nav.no>
2022-09-29 09:37:16 +0200

    :wastebasket: fjern unødvendig try/catch

--- hooks/useClientFetch.tsx
+++ hooks/useClientFetch.tsx
@@ -7,30 +7,26 @@ class FetchError extends Error {
 }
 const useClientFetch = () => {
   const clientFetch = async (url: string, method?: string, body?: any, skipStringify = false) => {
-    try {
-      const res = await fetch(url, {
-        method: method || 'GET',
-        ...(skipStringify ? { body } : body ? { body: JSON.stringify(body) } : {}),
-        redirect: 'manual',
-      });
-      if (res.status === 0) {
-        // eslint-disable-next-line max-len
-        const path = `https://aap-soknad.${process.env.NEXT_PUBLIC_NAV_HOSTNAME_URL}/oauth2/login?redirect=/aap/soknad/standard`;
-        if (window) {
-          // @ts-ignore
-          window.location = path;
-        }
-      } else if (!res.ok) {
-        const isJson = res.headers.get('content-type')?.includes('application/json');
-        const data = isJson ? await res.json() : null;
-        // get error message from body or default to response status
-        const error = new FetchError(`status: ${res.status}, msg: ${data?.message}`);
-        return Promise.reject(error);
+    const res = await fetch(url, {
+      method: method || 'GET',
+      ...(skipStringify ? { body } : body ? { body: JSON.stringify(body) } : {}),
+      redirect: 'manual',
+    });
+    if (res.status === 0) {
+      // eslint-disable-next-line max-len
+      const path = `https://aap-soknad.${process.env.NEXT_PUBLIC_NAV_HOSTNAME_URL}/oauth2/login?redirect=/aap/soknad/standard`;
+      if (window) {
+        // @ts-ignore
+        window.location = path;
       }
-      return res;
-    } catch (err) {
-      return Promise.reject(err);
+    } else if (!res.ok) {
+      const isJson = res.headers.get('content-type')?.includes('application/json');
+      const data = isJson ? await res.json() : null;
+      // get error message from body or default to response status
+      const error = new FetchError(`status: ${res.status}, msg: ${data?.message}`);
+      return Promise.reject(error);
     }
+    return res;
   };
   return { clientFetch };
 };
