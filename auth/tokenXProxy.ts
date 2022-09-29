de0450eea6e003eb4c43f8be3cc1afc0d4cab08a
Tor Idland <tor.idland@nav.no>
2022-09-29 10:44:47 +0200

    :construction: axiosproxy catch blokk

--- auth/tokenXProxy.ts
+++ auth/tokenXProxy.ts
@@ -86,7 +86,7 @@ export const tokenXAxiosProxy = async (opts: AxiosOpts) => {
     logger.info('Vellykket opplasting av fil til ' + opts.url);
     return data.pipe(opts.res);
   } catch (e: any) {
-    if (e.response.status === 422) {
+    if (e?.response?.status && e?.response?.status === 422) {
       e.response.data?.pipe(opts.res);
       return opts.res.status(422);
     }
