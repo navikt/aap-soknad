931f482632c59f244a3923e52f74a67440e751df
Tor Idland <tor.idland@nav.no>
2022-10-17 12:59:09 +0200

    :wrench: RUNTIME_ENVIRONMENT trenger ikke være NEXT_PUBLIC, brukes kun på server

--- utils/environments.ts
+++ utils/environments.ts
@@ -1,8 +1,8 @@
 const MOCK_ENVIRONMENTS = ['localhost', 'labs'];
 
-export const isLabs = () => ['labs'].includes(process.env.NEXT_PUBLIC_ENVIRONMENT ?? '');
+export const isLabs = () => ['labs'].includes(process.env.RUNTIME_ENVIRONMENT ?? '');
 
-export const isMock = () => MOCK_ENVIRONMENTS.includes(process.env.NEXT_PUBLIC_ENVIRONMENT ?? '');
+export const isMock = () => MOCK_ENVIRONMENTS.includes(process.env.RUNTIME_ENVIRONMENT ?? '');
 
 export const clientSideIsProd = () =>
   typeof window !== 'undefined' && window.location.href.includes('www.nav.no');
