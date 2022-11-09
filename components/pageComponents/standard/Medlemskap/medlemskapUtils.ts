9143ca8ff0dbed61a84f621f33a48565f8cbe6b1
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-11-09 11:00:21 +0100

    :bug: Validering og visning av spørsmål på medlemskap skal fungere bedre

---
+++ components/pageComponents/standard/Medlemskap/medlemskapUtils.ts
@@ -0,0 +1,32 @@
+import { JaEllerNei } from 'types/Generic';
+
+export const shouldShowArbeidetUtenforNorgeSiste5År = (boddINorge?: JaEllerNei) => {
+  return boddINorge === JaEllerNei.JA;
+};
+
+export const shouldShowArbeidetSammenhengendeINorgeSiste5År = (boddINorge?: JaEllerNei) => {
+  return boddINorge === JaEllerNei.NEI;
+};
+
+export const shouldShowITilleggArbeidetUtenforNorgeSiste5År = (
+  harArbeidetINorgeSiste5År?: JaEllerNei
+) => {
+  return harArbeidetINorgeSiste5År === JaEllerNei.JA;
+};
+
+export const shouldShowPeriodevelger = (
+  arbeidUtenforNorge?: JaEllerNei,
+  arbeidINorge?: JaEllerNei,
+  iTilleggArbeidUtenforNorge?: JaEllerNei
+) => {
+  if (arbeidUtenforNorge === JaEllerNei.JA) {
+    return true;
+  }
+  if (arbeidINorge === JaEllerNei.NEI) {
+    return true;
+  }
+  if (iTilleggArbeidUtenforNorge === JaEllerNei.JA) {
+    return true;
+  }
+  return false;
+};
