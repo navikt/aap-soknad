5e2ad485e674d855935db0aa836acd1988b4206a
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-11-10 15:17:25 +0100

    :arrow_up: Oppgraderer test-dependencies, fjerner cypress som ikke har vært i bruk på mange månede

--- cypress/integration/hovedsøknad.ts
+++
@@ -1,8 +0,0 @@
-describe('Søknad om AAP', () => {
-  it('skal kunne sende inn en enkel søknad', () => {
-    cy.visit('/aap');
-    cy.contains(/Søk om AAP/);
-    cy.findByRole('button', { name: /Søk AAP/ }).click();
-    cy.contains(/Takk, søknaden er mottatt. Nå tar vi oss av resten./);
-  });
-});
