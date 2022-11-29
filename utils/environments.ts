69561d217c8ee08a8cbee8554a82bd1dad085862
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-11-29 14:21:34 +0100

    :construction_worker: Tar i bruk ny env-variabel for å kjøre funksjonelle tester

--- utils/environments.ts
+++ utils/environments.ts
@@ -1,8 +1,12 @@
 const MOCK_ENVIRONMENTS = ['localhost', 'labs'];
 
-export const isLabs = () => ['labs'].includes(process.env.RUNTIME_ENVIRONMENT ?? '');
+export const isLabs = () =>
+  ['labs'].includes(process.env.RUNTIME_ENVIRONMENT ?? '') ||
+  process.env.FUNCTIONAL_TESTS === 'enabled';
 
-export const isMock = () => MOCK_ENVIRONMENTS.includes(process.env.RUNTIME_ENVIRONMENT ?? '');
+export const isMock = () =>
+  MOCK_ENVIRONMENTS.includes(process.env.RUNTIME_ENVIRONMENT ?? '') ||
+  process.env.FUNCTIONAL_TESTS === 'enabled';
 
 export const clientSideIsProd = () =>
   typeof window !== 'undefined' && window.location.href.includes('www.nav.no');
