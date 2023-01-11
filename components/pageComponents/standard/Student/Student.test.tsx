6aa835c5e16a6e454b96e34e9515efb673415112
Tor Idland <tor.idland@nav.no>
2023-01-11 14:58:14 +0100

    :white_check_mark: Student test for validering og visning av skjulte oppfølgingsspørsmål

--- components/pageComponents/standard/Student/Student.test.tsx
+++ components/pageComponents/standard/Student/Student.test.tsx
@@ -1,76 +1,63 @@
-import { StepWizardContext, StepWizardContextState } from 'context/stepWizardContextV2';
-import { findAllByRole, fireEvent, render, screen } from 'setupTests';
+import { findAllByRole, findByRole, fireEvent, renderStepSoknadStandard, screen } from 'setupTests';
 import { Step, StepWizard } from 'components/StepWizard';
 import { JaNeiVetIkke } from 'types/Generic';
 import React from 'react';
-import Student from './Student';
+import Student, { JaNeiAvbrutt } from './Student';
 import { axe, toHaveNoViolations } from 'jest-axe';
-import Soknad from 'types/Soknad';
-import { SoknadContextData } from 'context/soknadContextCommon';
-import { SøknadType } from 'types/SoknadContext';
-import { SoknadContextStandard } from 'context/soknadContextStandard';
-import { AppStateContext, AppStateContextState } from 'context/appStateContext';
+import messagesNb from 'translations/nb.json';
+import { act } from 'react-dom/test-utils';
 expect.extend(toHaveNoViolations);
 
 const STUDENT = 'student';
 
-const soknadContext: SoknadContextData<Soknad> = {
-  søknadState: {
-    version: 1,
-    type: SøknadType.STANDARD,
-  },
-  søknadDispatch: () => console.log('dispatch'),
-};
-const wizardContext: StepWizardContextState = {
-  stepList: [{ name: STUDENT }],
-  currentStep: { name: STUDENT },
-  currentStepIndex: 0,
-  stepWizardDispatch: () => ({}),
-};
-const appContext: AppStateContextState = {
-  appState: {
-    sistLagret: 'i dag',
-  },
-  appStateDispatch: () => console.log('dispatch'),
-};
-
-const renderWithContext = (ui: any, { ...renderOptions }: any) => {
-  return render(
-    <AppStateContext.Provider value={{ ...appContext }}>
-      <SoknadContextStandard.Provider value={{ ...soknadContext }}>
-        <StepWizardContext.Provider value={{ ...wizardContext }}>{ui}</StepWizardContext.Provider>,
-      </SoknadContextStandard.Provider>
-    </AppStateContext.Provider>,
-    renderOptions
-  );
-};
 describe('Student', () => {
   window.HTMLElement.prototype.scrollIntoView = function () {};
   const Component = () => {
     return (
       <StepWizard>
         <Step name={STUDENT}>
-          <Student onBackClick={jest.fn()} onCancelClick={jest.fn()} />
+          <Student onBackClick={jest.fn()} onNext={jest.fn()} />
         </Step>
       </StepWizard>
     );
   };
 
   it('Feil utfylt', async () => {
-    renderWithContext(<Component />, {});
-
+    renderStepSoknadStandard(STUDENT, <Component />, {});
     fireEvent.submit(screen.getByRole('button', { name: 'Neste steg' }));
     const errorSummary = await screen.findByRole('alert');
 
     expect(await findAllByRole(errorSummary, 'link')).toHaveLength(1);
   });
   it('Riktig utfylt - enkleste vei', async () => {
-    renderWithContext(<Component />, {});
-    fireEvent.click(screen.getByRole('radio', { name: JaNeiVetIkke.NEI }), {});
+    renderStepSoknadStandard(STUDENT, <Component />, {});
+    fireEvent.click(screen.getByRole('radio', { name: JaNeiAvbrutt.NEI }), {});
     expect(await screen.queryByRole('region')).toBeNull();
   });
+  it('Avbrutt studie - vis oppfølgingsspørsmål', async () => {
+    renderStepSoknadStandard(STUDENT, <Component />, {});
+    const avbruttLabel = messagesNb?.søknad?.student?.erStudent?.avbrutt;
+    fireEvent.click(screen.getByRole('radio', { name: avbruttLabel }), {});
+    const kommetilbakeTilStudietLegend = messagesNb?.søknad?.student?.kommeTilbake?.legend;
+    const oppfølgingsspørsmål = screen.getByRole('group', { name: kommetilbakeTilStudietLegend });
+    expect(oppfølgingsspørsmål).not.toBeNull();
+  });
+  it('Avbrutt studie - oppfølgingsspørsmål påkrevd', async () => {
+    renderStepSoknadStandard(STUDENT, <Component />, {});
+    act(() => {
+      const avbruttLabel = messagesNb?.søknad?.student?.erStudent?.avbrutt;
+      fireEvent.click(screen.getByRole('radio', { name: avbruttLabel }), {});
+    });
+    act(() => {
+      fireEvent.submit(screen.getByRole('button', { name: 'Neste steg' }));
+    });
+
+    const errorSummary = await screen.findByRole('alert');
+    const påkrevdTekst = messagesNb?.søknad?.student?.kommeTilbake?.required;
+    expect(await findByRole(errorSummary, 'link', { name: påkrevdTekst })).not.toBeNull();
+  });
   it('UU', async () => {
-    const { container } = renderWithContext(<Component />, {});
+    const { container } = renderStepSoknadStandard(STUDENT, <Component />, {});
     expect(await axe(container)).toHaveNoViolations();
   });
 });
