20a673d2634f335e1dae5ac673bc8db0b59959ac
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-01-20 09:33:41 +0100

    ✨ Legger til FileCard og FileCardError

---
+++ components/input/FileInput/FileCard.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import * as styles from './FileInput.module.css';
+import { Delete, FileSuccess } from '@navikt/ds-icons';
+import { BodyShort, Detail, Link, Panel } from '@navikt/ds-react';
+import { Vedlegg } from '../../../types/Soknad';
+
+interface Props {
+  vedlegg: Vedlegg;
+  remove: () => void;
+}
+
+export const FileCard = (props: Props) => {
+  const { vedlegg, remove } = props;
+
+  return (
+    <Panel className={styles.fileCard}>
+      <div className={styles?.fileCardLeftContent}>
+        <div className={styles?.fileSuccess}>
+          <FileSuccess color={'var(--a-icon-success)'} />
+        </div>
+        <div>
+          <Link target={'_blank'} href={`/aap/soknad/vedlegg/${vedlegg?.vedleggId}`}>
+            {vedlegg?.name}
+          </Link>
+          <Detail>{fileSizeString(vedlegg?.size)}</Detail>
+        </div>
+      </div>
+      <button
+        type={'button'}
+        onClick={() =>
+          fetch(`/aap/soknad/api/vedlegg/slett/?uuids=${vedlegg?.vedleggId}`, {
+            method: 'DELETE',
+          }).then(() => {
+            remove();
+          })
+        }
+        tabIndex={0}
+        onKeyPress={(event) => {
+          if (event.key === 'Enter') {
+            remove();
+          }
+        }}
+        className={styles?.deleteAttachment}
+      >
+        <Delete title={'Slett'} />
+        <BodyShort>{'Slett'}</BodyShort>
+      </button>
+    </Panel>
+  );
+};
+
+function fileSizeString(size: number) {
+  const kb = size / 1024;
+  return kb > 1000 ? `${(kb / 1024).toFixed(1)} mB` : `${Math.floor(kb)} kB`;
+}
---
+++ components/input/FileInput/FileCardError.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { BodyShort, Label, Panel } from '@navikt/ds-react';
+import * as classes from './FileInput.module.css';
+import { Cancel, FileError } from '@navikt/ds-icons';
+import { Vedlegg } from '../../../types/Soknad';
+
+interface Props {
+  vedlegg: Vedlegg;
+  remove: () => void;
+}
+
+export const FileCardError = (props: Props) => {
+  const { vedlegg, remove } = props;
+
+  return (
+    <>
+      <Panel className={`${classes?.fileCard} ${classes?.error}`} tabIndex={0}>
+        <div className={classes?.fileCardLeftContent}>
+          <div className={classes?.fileError}>
+            <FileError color={'var(--a-surface-danger-hover)'} />
+          </div>
+          <div>
+            <Label as={'span'}>{vedlegg.name}</Label>
+          </div>
+        </div>
+        <button
+          type={'button'}
+          onClick={() => {
+            remove();
+          }}
+          tabIndex={0}
+          onKeyPress={(event) => {
+            if (event.key === 'Enter') {
+              remove();
+            }
+          }}
+          className={classes?.deleteAttachment}
+        >
+          <Cancel title={'Avbryt'} />
+          <BodyShort>{'Avbryt'}</BodyShort>
+        </button>
+      </Panel>
+    </>
+  );
+};
