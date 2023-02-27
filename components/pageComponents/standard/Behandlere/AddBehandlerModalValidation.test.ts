df6c87a141075b83de4945f705c0ad30431da0b6
VHollund <vetlehoo@gmail.com>
2023-02-27 14:44:40 +0100

    tester for add behandler modal

--- components/pageComponents/standard/Behandlere/AddBehandlerModalValidation.test.ts
+++ components/pageComponents/standard/Behandlere/AddBehandlerModalValidation.test.ts
@@ -7,4 +7,26 @@ describe('AddBehandlerModal validation', () => {
     const result = await schema.validate({}, { abortEarly: false }).catch((err) => err);
     expect(result.errors.length).not.toBe(0);
   });
+  it('registrert behandler men fullt navn', async () => {
+    const form = {
+      firstname: 'chris',
+      lastname: 'nordperson',
+    };
+    const result = await schema.validate(form, { abortEarly: false }).catch((err) => err);
+    expect(result).toStrictEqual(form);
+  });
+  it('registrert behandler, kun fornavn', async () => {
+    const form = {
+      firstname: 'chris',
+    };
+    const result = await schema.validate(form, { abortEarly: false }).catch((err) => err);
+    expect(result.errors.length).not.toBe(0);
+  });
+  it('registrert behandler, kun etternavn', async () => {
+    const form = {
+      etternavn: 'chris',
+    };
+    const result = await schema.validate(form, { abortEarly: false }).catch((err) => err);
+    expect(result.errors.length).not.toBe(0);
+  });
 });
