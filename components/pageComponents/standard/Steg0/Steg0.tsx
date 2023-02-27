5c0911e971f9f6b7162e5e0f524fa9f078556b2f
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2023-02-27 13:34:09 +0100

    :children_crossing: Viser bekreftelse på steg0

--- components/pageComponents/standard/Steg0/Steg0.tsx
+++ components/pageComponents/standard/Steg0/Steg0.tsx
@@ -10,6 +10,8 @@ import { IntroduksjonTekst } from '../../../IntroduksjonTekst/IntroduksjonTekst'
 import { SokerOppslagState } from '../../../../context/sokerOppslagContext';
 import { getFulltNavn } from '../../../../lib/søker';
 import * as classes from './Steg0.module.css';
+import { Alert, BodyShort, Label } from '@navikt/ds-react';
+import { Success, SuccessColored } from '@navikt/ds-icons';
 
 interface Props {
   onNext: () => void;
@@ -33,6 +35,16 @@ export const Steg0 = ({ onNext, søker }: Props) => {
       cancelButtonText={formatMessage('navigation.cancel')}
     >
       <IntroduksjonTekst navn={getFulltNavn(søker.søker)} />
+
+      <div>
+        <Label as="p" spacing>
+          {formatMessage('søknad.veiledning.veiledningConfirm.title')}
+        </Label>
+        <div style={{ display: 'flex', gap: '0.25rem', alignItems: 'center' }}>
+          <SuccessColored />
+          <BodyShort>{formatMessage('søknad.veiledning.veiledningConfirm.label')}</BodyShort>
+        </div>
+      </div>
     </SøknadFormWrapper>
   );
 };
