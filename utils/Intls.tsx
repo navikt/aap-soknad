67913168940eae54487ec29de4317bbdf786e93b
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-06-23 22:39:00 +0200

    :sparkles: Legger til typet react-intl

---
+++ utils/Intls.tsx
@@ -0,0 +1,39 @@
+import type { ReactNode } from 'react';
+import type { IntlFormatters } from 'react-intl';
+import type { Props as ReactIntlFormattedMessageProps } from 'react-intl/src/components/message';
+import { FormattedMessage as ReactIntlFormattedMessage, useIntl as useReactIntl } from 'react-intl';
+
+import messagesNn from '../translations/nn.json';
+import messagesNb from '../translations/nb.json';
+import links from '../translations/links.json';
+
+type IntlMessageKeys = keyof typeof messagesNn | keyof typeof messagesNb | keyof typeof links;
+
+type FormatMessageArgs = Parameters<IntlFormatters<ReactNode>['formatMessage']>;
+
+type FormattedMessageProps = ReactIntlFormattedMessageProps<Record<string, ReactNode>> & {
+  id?: IntlMessageKeys;
+};
+
+export function FormattedMessage({ id, ...rest }: FormattedMessageProps) {
+  return <ReactIntlFormattedMessage id={id} {...rest} />;
+}
+
+export function useIntl() {
+  const { formatMessage, ...rest } = useReactIntl();
+
+  const typedFormatMessage = (
+    descriptor: FormatMessageArgs[0] & {
+      id?: IntlMessageKeys;
+    },
+    values?: FormatMessageArgs[1],
+    options?: FormatMessageArgs[2]
+  ) => {
+    return formatMessage(descriptor, values, options);
+  };
+
+  return {
+    ...rest,
+    formatMessage: typedFormatMessage,
+  };
+}
