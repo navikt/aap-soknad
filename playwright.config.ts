edeb941bd86fe45d447d7221be97f8e36750af19
Tor Idland <tor.idland@nav.no>
2026-03-13 15:34:05 +0100

    playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -99,7 +99,7 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'cp -r .next/static .next/standalone/aap-soknad/.next/static && cp -r public .next/standalone/aap-soknad/public 2>/dev/null; FUNCTIONAL_TESTS=enabled node .next/standalone/aap-soknad/server.js',
+    command: 'FUNCTIONAL_TESTS=enabled yarn start',
     port: 3000,
   },
 };
